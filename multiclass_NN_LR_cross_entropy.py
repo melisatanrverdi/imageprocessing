c16d519f69cb3d9887211d10b91b9b5739119e08
melisatanrverdi <melisatanrverdi@gmail.com>
2021-01-09 01:25:50 +0300

    Add files via upload

---
+++ multiclass_NN_LR_cross_entropy.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[45]:
+
+
+from sklearn.datasets import fetch_openml
+dataset = fetch_openml("mnist_784")
+
+
+# In[55]:
+
+
+type(dataset), type(dataset["data"]), type(dataset["target"])
+
+
+# In[56]:
+
+
+X, y = dataset["data"], dataset["target"]
+
+
+# In[58]:
+
+
+X = X / 255.0
+
+
+# In[59]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[60]:
+
+
+i = 10010
+img_1 = X[i,:].reshape(28,28)
+plt.imshow(img_1, cmap='gray')
+plt.title(y[i])
+plt.show()
+
+
+# In[62]:
+
+
+import numpy as np
+
+
+# In[64]:
+
+
+digits = 10
+examples = y.shape[0]
+
+y = y.reshape(1, examples)
+
+Y_new = np.eye(digits)[y.astype('int32')]
+Y_new = Y_new.T.reshape(digits, examples)
+
+
+# In[65]:
+
+
+Y_new.shape
+
+
+# In[66]:
+
+
+Y_new[:,100]
+
+
+# In[69]:
+
+
+y[0,100]
+
+
+# In[70]:
+
+
+m = 60000
+m_test = X.shape[0] - m
+    
+X_train, X_test = X[:m].T, X[m:].T
+Y_train, Y_test = Y_new[:,:m], Y_new[:,m:]
+
+shuffle_index = np.random.permutation(m)
+X_train, Y_train = X_train[:,shuffle_index], Y_train[:,shuffle_index]
+
+
+# In[38]:
+
+
+import matplotlib
+
+i = 12
+plt.imshow(X_train[:,i].reshape(28,28), cmap = 'gray')
+plt.axis("off")
+plt.show()
+Y_train[:,i]
+
+
+# In[76]:
+
+
+def compute_multiclass_loss(Y, Y_hat):
+    L_sum = np.sum(np.multiply(Y, np.log(Y_hat)))
+    m = Y.shape[1]
+    L = -(1/m) * L_sum
+    
+    return L
+
+
+# In[77]:
+
+
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+# In[78]:
+
+
+def compute_loss(Y, Y_hat):
+    
+    m = Y.shape[1]
+    L = -(1./m) * ( np.sum( np.multiply(np.log(Y_hat + epsilon), Y) ) + np.sum( np.multiply(np.log(1-Y_hat + epsilon),(1-Y)) ) )
+    return L
+
+
+# In[ ]:
+
+
+n_x = X_train.shape[0]
+n_h = 64
+learning_rate = 1
+
+W1 = np.random.randn(n_h, n_x)
+b1 = np.zeros((n_h, 1))
+W2 = np.random.randn(digits, n_h)
+b2 = np.zeros((digits, 1))
+
+X = X_train
+Y = Y_train
+
+for i in range(2000):
+
+    Z1 = np.matmul(W1,X) + b1
+    A1 = sigmoid(Z1)
+    Z2 = np.matmul(W2,A1) + b2
+    A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
+
+    cost = compute_multiclass_loss(Y, A2)
+
+    dZ2 = A2-Y
+    dW2 = (1./m) * np.matmul(dZ2, A1.T)
+    db2 = (1./m) * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.matmul(W2.T, dZ2)
+    dZ1 = dA1 * sigmoid(Z1) * (1 - sigmoid(Z1))
+    dW1 = (1./m) * np.matmul(dZ1, X.T)
+    db1 = (1./m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+
+    if (i % 100 == 0):
+        print("Epoch", i, "cost: ", cost)
+
+print("Final cost:", cost)
+
+
+# In[ ]:
+
+
+Z1 = np.matmul(W1, X_test) + b1
+A1 = sigmoid(Z1)
+Z2 = np.matmul(W2, A1) + b2
+A2 = np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
+
+predictions = np.argmax(A2, axis=0)
+labels = np.argmax(Y_test, axis=0)
+
+print(confusion_matrix(predictions, labels))
+print(classification_report(predictions, labels))
+
+
+# In[ ]:
+
+
+
+
